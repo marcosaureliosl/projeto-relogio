5042e6380ed77d80aba1ccfaac31a15e8ce9c719
Marcos Aurelio <127764997+marcosaureliosl@users.noreply.github.com>
2023-03-25 20:47:27 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const sugundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time() {
+    let datetoday = new Date ();
+    let  hr = datetoday.getHours();
+    let min = datetoday.getMinutes();
+    let s = datetoday.getSeconds();
+
+    if(hr < 10) hr = '0' + hr;
+
+    if(hr < 10) hr = '0' + min;
+
+    if(hr < 10) hr = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    sugundos.textContent=s;
+
+
+})
